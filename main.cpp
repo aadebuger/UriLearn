9e6a89651fbed36f3cf7b9237bd35063f31fb2c0
hua zhuang <aadebuger@huas-MacBook-Pro.local>
2016-12-01 15:14:24 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <utility>
+#include <network/uri.hpp>
+
+int main(int argc, char* argv[])
+{
+    std::error_code ec;
+    network::uri uri = network::make_uri("I am not a valid URI.", ec);
+    return 1;
+}
